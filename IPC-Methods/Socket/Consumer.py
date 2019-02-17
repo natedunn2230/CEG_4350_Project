8a206de534e3271751c8320a33af5e462cb09a3e
natedunn2230 <dunn.113@wright.edu>
2019-02-17 14:31:48 -0500

    Added socket IPC

---
+++ IPC-Methods/Socket/Consumer.py
@@ -0,0 +1,28 @@
+import socket
+import struct
+
+HOST = "127.0.0.1"
+PORT = 8082
+
+# consumer socket settings
+consumer_socket = socket.socket()
+consumer_socket.connect((HOST, PORT))
+
+# packer is used to unpack binary data into tuple
+binary_packer = struct.Struct('I')
+
+f = open("ConsumerOutput.txt", "w")
+
+while True:
+    # recieve data in chunks. Each chunk is the size of 
+    # the packed integer tuple
+    raw_data = consumer_socket.recv(binary_packer.size)
+    if not raw_data:
+        break
+
+    # extract the integer from the upacked tuple
+    (data ,) = binary_packer.unpack(raw_data)
+    
+    # write value to file to compare with producer to verify 
+    # correct communication
+    f.write("{}\n".format(data))
---
+++ IPC-Methods/Socket/Producer.py
@@ -0,0 +1,49 @@
+import socket
+import sys
+import struct
+import random
+
+HOST = "127.0.0.1"
+PORT = 8082
+DATA_SIZE = 100
+
+producer_socket = socket.socket()
+
+# producer socket settins
+producer_socket.settimeout(10)
+producer_socket.bind((HOST, PORT))
+producer_socket.listen(1)
+
+# file for printing generated data to
+producer_file = None
+
+while True:
+    
+    producer_file = open("ProducerOutput.txt", "w")
+
+    # wait for consumer connection, timeout after 10 seconds
+    try:
+        client, address = producer_socket.accept()
+        
+    except Exception:
+        print("Connection timed out")
+        break
+
+    print("Connected to {}".format(address))
+    
+    # send 100 random integers (range 1, 10000) to the consumer process
+    for i in range(DATA_SIZE):
+        data = random.randint(0, 10000)
+
+        # convert data into binary data to be sent over connection
+        raw_data = struct.Struct('I').pack((data))
+        client.send(raw_data)
+
+        # write the integer to a file to compare with the consumer to
+        # verify correct communication
+        producer_file.write("{}\n".format(data))
+
+    # close connection with consumer after data is sent
+    client.close()
+
+
